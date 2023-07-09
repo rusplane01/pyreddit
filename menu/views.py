7b328e2b56d358139d92b922869b367538ac3382
dabryboy <112767864+dabryboy@users.noreply.github.com>
2023-07-09 13:32:43 +0300

    добавили отображение постов и сделали структуру

--- menu/views.py
+++ menu/views.py
@@ -1,11 +1,23 @@
+from typing import Any, Dict
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Category
+from django.views.generic import ListView, DetailView
+from .models import Category, Post
 
 def index(request):
-    return render(  request, 'source.html', {'Title': 'pyreddit' }) 
+    return render(  request, 'menu/source.html', {'Title': 'pyreddit' }) 
+
+
+class ForumView(DetailView):
+    template_name = 'menu/forum.html'
+    model = Category
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(category = kwargs['object'])
+        
+        return context
+
 
 class CategoryView(ListView):
-    template_name = 'categories.html'
+    template_name = 'menu/categories.html'
     model = Category
 
